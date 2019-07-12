920a08dfb6d4357a78dfab3f4aa8271e7da98a60
awaffle1 <51020829+awaffle1@users.noreply.github.com>
2019-07-11 21:36:29 -0400

    Finished WGTAG for adventuregame 0.3

--- windows-guide-to-adventuregame.bat
+++ windows-guide-to-adventuregame.bat
@@ -1,2 +1,16 @@
 @echo off
-echo Coming soon :D
+echo A guide to adventuregame. - WINDOWS EXCLUSIVE EDITION - For adventuregame-python 0.3
+echo.
+echo.
+echo It's a basic game really,
+echo you read the text and then answer the question at the bottom.
+echo Sometimes the game is simple, sometimes the game is complex.
+echo To play the game, you just need to know how to read and type on a keyboard.
+echo I mean, that's pretty much it.
+echo.
+echo Homeuser3 Studios 2019 (est. 2017)
+echo.
+echo Sidenote- Dang, it's been a long time since I've coded in Batch lol
+echo So just follow the instruction below and leave here. There's not much to see anyways.
+pause
+exit
